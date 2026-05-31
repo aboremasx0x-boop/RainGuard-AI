9ae7a16e56cddfdcbd2dd9f152ffef9317a80b04
aboremasx0x-boop <aboremasx0x@gmail.com>
2026-05-31 15:52:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,9 @@ const FLOOD_RISK_HIGH_SCORE = 80;
 const FLOOD_RISK_MEDIUM_SCORE = 60;
 const FLOOD_RISK_WATCH_SCORE = 40;
 
+let floodMapLayer = [];
+let floodMapEnabled = true;
+
 const smartMultiCityMonitorList = [
     { name: "جدة", lat: 21.5433, lon: 39.1728 },
     { name: "مكة", lat: 21.3891, lon: 39.8579 },
