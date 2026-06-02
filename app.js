8046dfb5c23976883775634bd94c2e2a8b88483d
aboremasx0x-boop <aboremasx0x@gmail.com>
2026-06-03 01:39:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1625,6 +1625,7 @@ function clearSmartMultiCityHistory() {
 window.lastMultiCityResults = [];
 
 function openCityForecastPopup(cityName) {
+    closeCityForecastPopup();
     const city = window.lastMultiCityResults.find(c => c.name === cityName);
 
     if (!city) {
