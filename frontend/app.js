1ba0e5f50e9bfe5e8d008ec45fd9ee2b7ea0b4be
aboremasx0x-boop <aboremasx0x@gmail.com>
2026-06-15 03:37:54 +0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1699,21 +1699,6 @@ function renderSmartMultiCityTopPanel(results) {
     }).join("");
 }
 
-const uniqueCities = [];
-const used = new Set();
-
-floodCities.forEach(city => {
-    const key =
-        Math.round(Number(city.lat) * 10) +
-        "_" +
-        Math.round(Number(city.lon) * 10);
-
-    if (!used.has(key)) {
-        used.add(key);
-        uniqueCities.push(city);
-    }
-});
-
 function renderFloodWatchCitiesPanel(results) {
     const box = document.getElementById("floodWatchCitiesBox");
     if (!box) return;
@@ -1753,7 +1738,20 @@ function renderFloodWatchCitiesPanel(results) {
         `;
         return;
     }
+    const uniqueCities = [];
+const used = new Set();
 
+floodCities.forEach(city => {
+    const key =
+        Math.round(Number(city.lat) * 10) +
+        "_" +
+        Math.round(Number(city.lon) * 10);
+
+    if (!used.has(key)) {
+        used.add(key);
+        uniqueCities.push(city);
+    }
+});
     box.innerHTML = uniqueCities.slice(0, 8).map(city => {
         const floodScore = Number(city.floodRiskScore || 0);
         const rainScore = Number(city.score || 0);
