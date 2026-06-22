67ce6afdb20dcdc29dd71a4d5c61847ebf62bcce
aboremasx0x-boop <aboremasx0x@gmail.com>
2026-06-22 10:23:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -836,18 +836,6 @@ async def get_actual_rain_from_open_meteo(lat, lon, prediction_time):
         if lat is None or lon is None or not prediction_time:
             return 0.0
 
-        @app.get("/test-archive")
-async def test_archive():
-    rain = await get_actual_rain_from_open_meteo(
-        21.5433,
-        39.1728,
-        "2026-06-01T12:00:00"
-    )
-
-    return {
-        "actual_rain": rain
-    }
-
         raw_time = str(prediction_time).replace("Z", "")
         dt = datetime.fromisoformat(raw_time)
 
@@ -2127,6 +2115,18 @@ def ai_status():
         "adaptive_learning": adaptive_learning_v1(),
         "adaptive_thresholds": adaptive_thresholds_v2(),
         "message": "RainGuard AI learning system is active"
+    }
+
+@app.get("/test-archive")
+async def test_archive():
+    rain = await get_actual_rain_from_open_meteo(
+        21.5433,
+        39.1728,
+        "2026-06-01T12:00:00"
+    )
+
+    return {
+        "actual_rain": rain
     }
 
 
